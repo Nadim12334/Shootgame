013506edf75ab47bec90ed394e82bce4400f32bd
Nadim12334 <140494988+Nadim12334@users.noreply.github.com>
2024-11-09 11:40:46 +0530

    Initial commi

---
+++ shootgame.js
@@ -0,0 +1,32 @@
+let score=  document.getElementById("showScore");
+let sound=document.getElementById("playSound");
+function white(){
+  document.getElementById("showScore")
+  score.innerHTML=  "Score :"+10;
+  sound.play();
+}
+function black(){
+  document.getElementById("showScore")
+  score.innerHTML=  "Score :" +20;
+  sound.play();
+}
+function blue(){
+  document.getElementById("showScore")
+  score.innerHTML=  "Score :" +40;
+  sound.play();
+}
+function red(){
+  document.getElementById("showScore")
+  score.innerHTML=  "Score :" +60;
+  sound.play();
+}
+function yellow(){
+  document.getElementById("showScore")
+  score.innerHTML=  "Score :" +80;
+  sound.play();
+}
+function green(){
+  document.getElementById("showScore")
+  score.innerHTML=  "Score :" +100;
+  sound.play();
+}
